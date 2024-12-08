a2857058af1eec45281fdee6795615531b84b585
Ruben Gees <rubengees7@gmail.com>
2024-12-08 11:34:57 +0100

    Optimize part 2

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -2,6 +2,27 @@ import { lines } from "../utils.ts"
 
 export function part1(input: string): string {
   const map = lines(input).map((line) => line.split(""))
+  const marked = simulate(map)
+
+  return marked
+    .flat()
+    .filter((it) => it)
+    .length.toString()
+}
+
+function findStart(map: string[][]) {
+  for (let x = 0; x < map.length; x++) {
+    for (let y = 0; y < map[x].length; y++) {
+      if (map[x][y] === "^") {
+        return [x, y]
+      }
+    }
+  }
+
+  throw new Error("No start found")
+}
+
+function simulate(map: string[][]) {
   const marked = map.map((line) => line.map(() => false))
 
   let [x, y] = findStart(map)
@@ -23,62 +44,56 @@ export function part1(input: string): string {
   }
 
   return marked
-    .flat()
-    .filter((it) => it)
-    .length.toString()
-}
-
-function findStart(map: string[][]) {
-  for (let x = 0; x < map.length; x++) {
-    for (let y = 0; y < map[x].length; y++) {
-      if (map[x][y] === "^") {
-        return [x, y]
-      }
-    }
-  }
-
-  throw new Error("No start found")
 }
 
 export function part2(input: string): string {
   const map = lines(input).map((line) => line.split(""))
   const [startX, startY] = findStart(map)
+  const path = simulate(map)
 
   let loopCount = 0
 
-  for (let obstacleX = 0; obstacleX < map.length; obstacleX++) {
-    for (let obstacleY = 0; obstacleY < map[0].length; obstacleY++) {
-      const visited = new Set<string>()
+  for (let obstacleX = 0; obstacleX < path.length; obstacleX++) {
+    for (let obstacleY = 0; obstacleY < path[obstacleX].length; obstacleY++) {
+      if (!path[obstacleX][obstacleY]) continue
 
-      let [x, y] = [startX, startY]
-      let [dX, dY] = [-1, 0]
-
-      while (true) {
-        const key = `${x}|${y}|${dX}|${dY}`
-
-        if (visited.has(key)) {
-          loopCount++
-          break
-        }
-
-        visited.add(key)
-
-        const [nextX, nextY] = [x + dX, y + dY]
-        const next = map[nextX]?.[nextY]
-
-        if (next === undefined) {
-          break
-        }
-
-        if (next === "#" || (nextX === obstacleX && nextY === obstacleY)) {
-          ;[dX, dY] = [dY, -dX]
-        } else {
-          x = nextX
-          y = nextY
-        }
-      }
+      loopCount += simulate2(map, startX, startY, obstacleX, obstacleY)
     }
   }
 
   return loopCount.toString()
+}
+
+function simulate2(map: string[][], startX: number, startY: number, obstacleX: number, obstacleY: number) {
+  const visited = new Set<number>()
+
+  let x = startX
+  let y = startY
+  let dX = -1
+  let dY = 0
+
+  while (true) {
+    const nextX = x + dX
+    const nextY = y + dY
+    const next = map[nextX]?.[nextY]
+
+    if (next === undefined) return 0
+
+    if (next === "#" || (nextX === obstacleX && nextY === obstacleY)) {
+      const key = ((x + 1) << 24) | ((y + 1) << 16) | ((dX + 1) << 8) | (dY + 1)
+
+      if (visited.has(key)) return 1
+
+      visited.add(key)
+
+      const newDx = dY
+      const newDy = -dX
+
+      dX = newDx
+      dY = newDy
+    } else {
+      x = nextX
+      y = nextY
+    }
+  }
 }
